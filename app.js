1dba59ba4cb9881085d60b9e5fceccd52beda6f2
PEDRO HENRIQUE OLIVEIRA DA SILVA <25132766@CFP127ALUNO.LOCAL>
2025-09-15 16:24:17 -0300

    Criando .gitiginore

---
+++ app.js
@@ -0,0 +1,9 @@
+// objetivo: API responsavel em criar endPoints referente esatdos e cidades
+// data: 15-09-2025
+// autor: Pedro Henrique Oliveira da Silva
+// versão 1.0
+//
+// obs: instalar dependencias para criar api
+//      express - npm install express          --save
+//      cors    - npm install cors             --save
+//      body-parser - npm install  body-parser --save
