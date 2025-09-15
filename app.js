9a4f11048705f6968bb4f8cf05dba49624b98c44
PEDRO HENRIQUE OLIVEIRA DA SILVA <25132766@CFP127ALUNO.LOCAL>
2025-09-15 17:02:21 -0300

    configurando cors e importando dependências

--- app.js
+++ app.js
@@ -4,6 +4,28 @@
 // versão 1.0
 //
 // obs: instalar dependencias para criar api
-//      express - npm install express          --save
-//      cors    - npm install cors             --save
-//      body-parser - npm install  body-parser --save
+//      express - npm install express          --save isntala dependencias para criar uma api 
+//      cors    - npm install cors             --save instala as dependencias para configurar as permissões de api
+//      body-parser - npm install  body-parser --save  instala as dependencias para receber os tipos de dados via post ou put
+
+//import das dependencias
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+//deifine a porta padrão da api, se for servidor de nuvem nao temos acesso a porta
+    //EM EXECUÇÃO LOCAL PODEMOS DEFINIR A PORTA
+const PORT  = process.PORT || 8080
+
+//instancia da classe express
+const app = express()
+
+//
+app.use((request,response,next)=>{
+    response.header('Acess-Control-Allow-Origin', '0.0.0.0')
+    response.header('Acess-Control-Allow-Methods', 'GET')
+
+    app.use(cors())
+    next()
+})
+
+app.get('/')
